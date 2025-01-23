84915626dcaecf8537ed7de1661d7a6c7d41b7d6
Maksym Veretnov <maxvertnov@gmail.com>
2025-01-23 20:47:13 +0100

    solve hw tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,20 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
+// debugger;
+function filterArray(numbers, value) {
+  const newArr = [];
+  for (const element of numbers) {
+    if (element > value) {
+      newArr.push(element);
+    }
+  }
+  return newArr;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+// function filterArray(numbers, value) {
+//   return numbers.filter(element => element > value);
+// }
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
